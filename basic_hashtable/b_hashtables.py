7ef4ae5f5b730738f0ac3a22c40ad19cb7e87209
Stephanie Patton <stephaniepatton@stephanies-air.attlocal.net>
2019-09-04 07:55:16 -0500

    finihed hash

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -16,7 +16,7 @@ class Pair:
 class BasicHashTable:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.storage = [None] * capacity
+        self.storage = [None] * capacity  # initializing to none
 
 # '''
 # Fill this in.
@@ -25,7 +25,14 @@ class BasicHashTable:
 
 
 def hash(string, max):
-    pass
+    hash = 5381
+    for char in string:
+        # ord returns unicode point/ascii value for single char
+        # << or >> is binary shifting
+        hash = ((hash << 5) + hash) + ord(char)
+
+    # returns a hashed integer between 0 and max
+    return hash % max
 
 
 # '''
@@ -34,23 +41,49 @@ def hash(string, max):
 # If you are overwriting a value with a different key, print a warning.
 # '''
 def hash_table_insert(hash_table, key, value):
-    pass
 
+    # hash the key, max = hash table capacity
+    index = hash(key, hash_table.capacity)  # where the pair object is going
+    print("Hashed Key: ", index)
+
+    # create a pair
+    pair = Pair(key, value)
+
+    # check if overwriting value with different key
+    if hash_table.storage[index]:  # if not empty
+        # if keys, not hashes, are different
+        if hash_table.storage[index].key != key:
+            print(
+                f"WARNING: You're overwriting the value at {hash_table.storage[index].key}.")
+
+    # add pair to hash table
+    hash_table.storage[index] = pair
 
 # '''
 # Fill this in.
 
 # If you try to remove a value that isn't there, print a warning.
 # '''
-def hash_table_remove(hash_table, key):
-    pass
 
+
+def hash_table_remove(hash_table, key):
+
+    # hash the key, max is hash table capacity
+    index = hash(key, hash_table.capacity)
+
+    if not hash_table.storage[index].key:
+        print(f"WARNING: {key} does not exist in hash table.")
+
+    # set new value of key to None
+    hash_table.storage[index] = None
 
 # '''
 # Fill this in.
 
 # Should return None if the key is not found.
 # '''
+
+
 def hash_table_retrieve(hash_table, key):
     pass
 
